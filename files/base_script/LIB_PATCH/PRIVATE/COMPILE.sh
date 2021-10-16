b8d3f795d32e32a5380027b6d4e9c234f56f7998
friyadhibiermann <friyadhibiermann@gmail.com>
2021-10-17 01:41:40 +0700

    first commit

--- files/base_script/LIB_PATCH/PRIVATE/COMPILE.sh
+++
@@ -1,9 +0,0 @@
-file="LIB_PATCHER"
-for i in $file;do
-cp -r $i.sh /data/data/com.termux/files/home/memedit_mysource/src/PROXCRYPT/
-echo "copy file $i.sh"
-cp -rf /data/data/com.termux/files/home/memedit_mysource/libs/arm64-v8a/$i ../${i}64
-echo "compile sucess ${i}64"
-cp -rf /data/data/com.termux/files/home/memedit_mysource/libs/armeabi-v7a/$i ../${i}32
-echo "compile sucess ${i}32"
-done
